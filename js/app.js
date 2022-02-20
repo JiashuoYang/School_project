d997191e99e9f35e5b6f7c29d550da688ad25a83
JiashuoYang <yangjiashuo69@gmail.com>
2022-02-20 20:04:24 +0800

    order

--- js/app.js
+++ js/app.js
@@ -1,23 +1,55 @@
 const app = angular.module("myApp", []);
-app.controller("myCtrl", ($scope, $http) => {
-    $scope.hostname = "http://cs102.nihs.tp.edu.tw:5000";
+app.controller("homeCtrl", ($scope, $http) => {
+    $scope.hostname = "http://localhost/web/School_project/pages/index.html";
+    $scope.api = "http://cs102.nihs.tp.edu.tw:5000";
     $scope.restaurants = [];
-    $scope.restaurant_logo = "";
-    $scope.restaurant_name = "";
+    $scope.current_restaurant = {
+        logo: "",
+        name: "",
+        rating: 0,
+        cuisines: [],
+        code: "",
+    };
     $scope.categories = [];
-    $scope.product = "";
+    $scope.meal = "";
+    $scope.products_selected = [];
+    $scope.amount = 0;
+    $scope.canOrder = false;
+    $scope.sortedMeals = [];
     $http
-        .get(`${$scope.hostname}/search?key`)
+        .get(`${$scope.api}/search?key`)
         .then((res) => {
             $scope.restaurants = res.data;
         })
         .catch((err) => {
             console.error(err);
         });
+    $scope.handleOrder = (order) => {
+        console.log(order);
+        switch (order) {
+            case "default":
+            default:
+                $scope.restaurants.sort((a, b) => a.id - b.id);
+                break;
+            case "fee":
+                $scope.restaurants.sort(
+                    (a, b) => a.minimum_delivery_fee - b.minimum_delivery_fee
+                );
+                break;
+            case "rating":
+                $scope.restaurants.sort((a, b) => b.rating - a.rating);
+                break;
+            case "time":
+                $scope.restaurants.sort(
+                    (a, b) => a.minimum_delivery_time - b.minimum_delivery_time
+                );
+                break;
+        }
+    };
     $scope.handleSearch = () => {
-        $scope.product = "";
+        $scope.meal = "";
         $http
-            .get(`${$scope.hostname}/search?key=${$scope.prodefct}`)
+            .get(`${$scope.api}/search?key=${$scope.meal}`)
             .then((res) => {
                 $scope.restaurants = res.data;
             })
@@ -27,13 +59,59 @@ app.controller("myCtrl", ($scope, $http) => {
     };
     $scope.chooseRestaurant = (restaurant) => {
         $http
-            .get(`${$scope.hostname}/restaurant?code=${restaurant.code}`)
+            .get(`${$scope.api}/restaurant?code=${restaurant.code}`)
             .then((res) => {
-                $scope.restaurant_logo = restaurant.hero_image;
-                $scope.restaurant_name = restaurant.name;
                 $scope.restaurants = [];
+                $scope.current_restaurant = {
+                    logo: restaurant.hero_image,
+                    name: restaurant.name,
+                    rating: restaurant.rating,
+                    cuisines: restaurant.cuisines,
+                    code: restaurant.code,
+                };
                 $scope.categories = res.data.menus[0].menu_categories;
-                console.log(res.data.menus[0].menu_categories);
+                $scope.amount = restaurant.minimum_delivery_fee;
+                $scope.canOrder = true;
+                for (let i = 0; i < $scope.categories.length; i++) {
+                    for (
+                        let j = 0;
+                        j < $scope.categories[i].products.length;
+                        j++
+                    ) {
+                        $scope.categories[i].products[j].selected = false;
+                        $scope.categories[i].products[j].count = 0;
+                    }
+                }
             });
+    };
+    $scope.SearchFilter = (e) => {
+        $scope.meal = e.meal;
+        if ($scope.meal.length > 0) {
+            $scope.sortedMeals = $scope.categories.filter((categorie) =>
+                categorie.products
+                    .map((product) => product.name)
+                    .includes($scope.meal)
+            );
+            console.log($scope.meal);
+        } else {
+            console.log("none of the above.");
+        }
+    };
+    $scope.chooseProduct = (product) => {
+        $scope.products_selected.push(product.name);
+        $scope.amount += product.product_variations[0].price;
+        console.log(
+            `餐點：${product.name}，價格：${product.product_variations[0].price}`
+        );
+        console.log(`合計：${$scope.amount}(含外送服務費)`);
+        product.selected = true;
+        product.count++;
+        // $http
+        //     .get(
+        //         `${$scope.api}/search_box?code=${$scope.restaurant_code}&box="${$scope.products}"`
+        //     )
+        //     .then((res) => {
+        //         console.log(res.data.answer);
+        //     });
     };
 });
